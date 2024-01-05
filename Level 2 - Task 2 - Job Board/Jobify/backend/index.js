1155f17702aa6f477c449b00d8877a621ccc63cd
Danish <danishsidd524@gmail.com>
2024-01-05 20:44:27 +0500

    added requirements for task

--- Level 2 - Task 2 - Job Board/Jobify/backend/index.js
+++ Level 2 - Task 2 - Job Board/Jobify/backend/index.js
@@ -2,7 +2,7 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
-import dotenv from "dotenv";
+import {config} from "dotenv";
 import "express-async-errors";
 // Config Imports
 import connDb from "./config/db.js";
@@ -18,7 +18,7 @@ import expMongoSanitize from "express-mongo-sanitize";
 
 
 // .env config
-dotenv.config();
+config();
 
 // Database connection
 connDb()
